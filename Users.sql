28782d7f8cd70b9809982872b4d75001335a3bb7
prcbass <cbasshernandez@gmail.com>
2016-12-04 13:42:46 -0500

    Change varchar size limits on emailAddress and passwordHash fields. Also change phoneNumber field from integer data type to varchar2(20)

--- Users.sql
+++ Users.sql
@@ -1,9 +1,9 @@
 create table UserAccount
-(emailAddress varchar2(25),
-passwordHash varchar2(25), 
+(emailAddress varchar2(100),
+passwordHash varchar2(255), 
 firstName varchar2(25), 
 lastName varchar2(25), 
-phoneNumber integer, 
+phoneNumber varchar2(20), 
 role varchar2(25), 
 addr1 varchar2(25), 
 addr2 varchar2(25), 
