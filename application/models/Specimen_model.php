a873dc08176898cc3489925e121295ad46605331
Warren Brown <protodrone@gmail.com>
2016-12-04 19:11:36 -0500

    Looking sharp. Filters tested, adding more formatting. TAXONDETERMINATION and COLLECTIONEVENT are joined.

--- application/models/Specimen_model.php
+++ application/models/Specimen_model.php
@@ -16,23 +16,31 @@ class Specimen_model extends CI_Model {
         public function search_specimen($filterArray,$execute_query = TRUE)
         {
                 $this->load->helper('url');
-                $this->db->limit(50);
+                $this->db->limit(25);
                 foreach ($filterArray as $f):
                         if ($f['operator'] == 'equal')
                                 $this->db->where($f['specimenTerm'],$f['criteria']);
+                        if ($f['operator'] == "not")
+                        {
+                                $t = $f['specimenTerm'] . "!=";
+                                $this->db->where($t,$f['criteria']);
+                        }
                         if ($f['operator'] == 'tuple')
-                                $this->db->where_in($f['specimenTerm'],$f['criteria']);
+                                $this->db->where_in($f['specimenTerm'],explode(",",$f['criteria']));
                         if ($f['operator'] == 'nottuple')
-                                $this->db->where_not_in($f['specimenTerm'],$f['criteria']);
+                                $this->db->where_not_in($f['specimenTerm'],explode(",",$f['criteria']));
                         if ($f['operator'] == 'like')
-                                $this->db->where_like($f['specimenTerm'],$f['criteria']);
+                                $this->db->like($f['specimenTerm'],$f['criteria']);
                         if ($f['operator'] == 'likeafter')
-                                $this->db->where_like($f['specimenTerm'],$f['criteria'],'after');
+                                $this->db->like($f['specimenTerm'],$f['criteria'],'after');
                         if ($f['operator'] == 'likebefore')
-                                $this->db->where_like($f['specimenTerm'],$f['criteria'],'before');
+                                $this->db->like($f['specimenTerm'],$f['criteria'],'before');
                 endforeach;
                 $this->db->from('SPECIMEN');
-                if ($execute_query === TRUE)
+                $this->db->join('TAXONDETERMINATION','SPECIMEN.DETERMINATIONID = TAXONDETERMINATION.DETERMINATIONID');
+                $this->db->join('COLLECTIONEVENT','SPECIMEN.COLLECTIONEVENTID = COLLECTIONEVENT.COLLECTIONEVENTID');
+
+                if ($execute_query === FALSE)
                 {
                         $query = $this->db->get_compiled_select();
                         return $query;
--- application/views/specimen_search.php
+++ application/views/specimen_search.php
@@ -10,6 +10,7 @@
       <div class="col-md-2">
         <select class="form-control" name="operator">
           <option value="equal"> = </option>
+          <option value="not"> != </option>
           <option value="tuple">in tuple</option>
           <option value="nottuple">not in tuple</option>
           <option value="like">contains</option>
@@ -30,34 +31,32 @@
       <div class="col-md-2">
         <input class="form-control" name="filterCriteria" />
       </div>
-      <div class="col-md-2">
-        <div class="row">
-          <input class="btn btn-primary left" type="submit" name="submit" id="submit" value="Add Filter" />
+        <div class="btn-group">
+          <input class="btn btn-group btn-primary" type="submit" name="submit" id="submit" value="Add Filter" />
+          <input class="btn btn group btn-primary" type="submit" name="run_search" id="run_search" value="Search" />
+          <input class="btn btn-group btn-danger" type="submit" name="reset_search" id="reset_search" value="Reset" />
         </div>
-      </div>
-      <div class="col-md-2">
-        <div class="row">
-          <input class="btn btn-primary left" type="submit" name="reset_search" id="reset_search" value="Reset" />
-        </div>
-    </div>
+    
   </div>
 
 <!-- BEGIN FILTER ARRAY -->
 <section>
   <div class="container">
     <h3>Filter Array</h3>
-    <input class="btn btn-primary left" type="submit" name="run_search" id="run_search" value="search" />
-    <?php isset($filterArray) ? print_r($filterArray):''; ?>
-    <table border="1" cellpadding="0" cellspacing="0">
-      <tr><th>Term</th><th>operator</th><th>Criteria</th></tr>
-      <?php foreach ($filterArray as $filter_item): ?>        
-        <tr>
-          <td><?php echo $filter_item['specimenTerm']; ?></td>
-          <td><?php echo $filter_item['operator']; ?></td>
-          <td><?php echo $filter_item['criteria']; ?></td>    
-      </tr>
-      <?php endforeach; ?>
-    </table>
+    <div class="row>"
+      <div class="column-sm-1">
+        <table class="table table-hover table-striped table-condensed">
+          <tr><th>Term</th><th>operator</th><th>Criteria</th></tr>
+          <?php foreach ($filterArray as $filter_item): ?>        
+            <tr>
+              <td><?php echo $filter_item['specimenTerm']; ?></td>
+              <td><?php echo $filter_item['operator']; ?></td>
+              <td><?php echo $filter_item['criteria']; ?></td>    
+          </tr>
+          <?php endforeach; ?>
+        </table>
+      </div>
+    </div>
   </div>
 </section>
 <!-- END FILTER ARRAY -->
@@ -66,9 +65,23 @@
 <section>
   <div class="container">
     <h3>Query Results</h3>
-    <?php if (isset($sql)) print_r($sql);  ?>
-    <?php //print_r($searchresult); ?>
-    <strong>[Put Tables Here]</strong>
+    <?php if (isset($sql)) echo "<p>$sql</p>";  ?>
+    <?php if (isset($searchresult) && count($searchresult) > 0): ?>
+      <table class="table table-hover table-stripped table-bordered table-condensed">
+        <thead>
+          <tr>
+            <th><?php echo implode('</th><th>', array_keys(current($searchresult))); ?></th>
+          </tr>
+        </thead>
+        <tbody>
+      <?php foreach ($searchresult as $row): array_map('htmlentities', $row); ?>
+          <tr>
+            <td><?php echo implode('</td><td>', $row); ?></td>
+          </tr>
+      <?php endforeach; ?>
+        </tbody>
+      </table>
+    <?php endif; ?>
   </div>
 </section>
 <!-- END QUERY RESULTS -->
