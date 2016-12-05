f44ad7206cf1772301ba5627436ca5b5d0c4f822
Cole Yonkee <cyonkee@gmail.com>
2016-12-05 16:54:08 -0500

    Add files via upload

---
+++ Countsview.php
@@ -0,0 +1,7 @@
+<?php
+  echo 'CollectionEvent Tuple Count: ' . $collecEvent . '<br>';
+  echo 'Locality Tuple Count: ' . $locality . '<br>';
+  echo 'PaleoContext Tuple Count: ' . $paleo . '<br>';
+  echo 'Specimen Tuple Count: ' . $specimen . '<br>';
+  echo 'TaxonDetermination Tuple Count: ' . $taxonD;
+?>
