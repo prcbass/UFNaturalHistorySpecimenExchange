d5f744a7d0320646b7b6c193285a6f51d85718e5
prcbass <cbasshernandez@gmail.com>
2016-12-06 10:45:07 -0500

    Remove unnecessary scripts from heaptmapfooter

--- application/views/templates/heatmapfooter.php
+++ application/views/templates/heatmapfooter.php
@@ -1,23 +1,6 @@
 
-<script
-        src="https://code.jquery.com/jquery-3.1.1.min.js"
-        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-        crossorigin="anonymous"></script>
-
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
-<script type="text/javascript">
-  $('select').select2();
-  $(".select-term").select2({
-    placeholder: "Select Term",
-    allowClear: true
-  });
-  $(".operator").select2({
-    placeholder: "Operator",
-    allowClear: true
-  });
-</script>
 <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDN2DW_TbwI3gNVPUeo8Pp4DMjnCSBCdYA&libraries=visualization&callback=initHeatMap">
 </script>
