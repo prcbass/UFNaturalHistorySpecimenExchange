0936baced11d94b385092b6cedf7abaa8cf176fc
prcbass <cbasshernandez@gmail.com>
2016-12-06 11:23:11 -0500

    Change Tuple count view to use html table

--- application/views/countsview.php
+++ application/views/countsview.php
@@ -1,9 +1,20 @@
 <div class="container">
-  <?php echo '<h3>CollectionEvent Tuple Count: ' . $collecEvent . '</h3>'; ?>
-  <?php echo '<h3>Locality Tuple Count: ' . $locality . '</h3>'; ?>
-  <?php echo '<h3>PaleoContext Tuple Count: ' . $paleo . '</h3>'; ?>
-  <?php echo '<h3>Specimen Tuple Count: ' . $specimen . '</h3>'; ?>
-  <?php echo '<h3>TaxonDetermination Tuple Count: ' . $taxonD . '</h3>'; ?>
-  <br>
-  <strong><h2>Total: <?php echo $totalCount ?></h2></strong>
+  <table class="table table-hover table-striped table-condensed">
+    <tr>
+      <th>Collection Event</th>
+      <th>Locality</th>
+      <th>Paleo Context</th>
+      <th>Specimen</th>
+      <th>Taxon Determination</th>
+      <th>Total</th>
+    </tr>
+    <tr>
+      <td><?php echo $collecEvent ?></td>
+      <td><?php echo $locality ?></td>
+      <td><?php echo $paleo ?></td>
+      <td><?php echo $specimen ?></td>
+      <td><?php echo $taxonD ?></td>
+      <td><?php echo $totalCount ?></td>
+    </tr>
+  </table>
 </div>
